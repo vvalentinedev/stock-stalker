f1afddf29b239c031e0bc34f87ac99a62b0ba384
April <everardo.business@proton.me>
2026-04-03 21:44:26 -0700

    Refactoring core.py and cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,1 +1,16 @@
-    
+import time
+
+class CLI:
+    interval: int = 60
+
+    def __init__(self, interval):
+        pass
+
+    def print_data(self, symbol, price_range, currency):
+        self._printInitial_header
+        while True:
+            print(f"")
+
+
+    def _printInitial_header():
+        print("===================")
--- src/core.py
+++ src/core.py
@@ -3,14 +3,13 @@ import os
 
 class Core:
     tickers_list = []
-    interval = 15
 
     def __init__(self):
-        self._setTickerList()
+        self._set_ticker_list()
         self._start()
 
 
-    def _setTickerList(self):
+    def _set_ticker_list(self):
         if os.path.exists("my_stock_list.csv") and os.path.getsize("my_stock_list.csv") > 0:
             with open("my_stock_list.csv") as file:
                 self.tickers_list = [line.strip() for line in file if line.strip()]
@@ -18,14 +17,15 @@ class Core:
             # if my_stock_list.csv doesn't exist or is empty, a default ticker list is loaded.
             self.tickers_list = ["AAPL", "NVDA", "GOOG"]
 
-    def _start(self):
+    def get_ticker_list(self): 
         for current_ticker in self.tickers_list:
             dat = yf.Ticker(current_ticker)
 
             symbol = dat.info["symbol"]
             price_range = dat.info["regularMarketDayRange"]
             currency = dat.info["financialCurrency"]
-            print(f"{symbol}: {price_range} {currency}")
+            # print(f"{symbol}: {price_range} {currency}")
+            return (symbol, price_range, currency)
 
              
 
