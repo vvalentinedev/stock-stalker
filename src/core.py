a41f1bb4fcc4a1f5917cfb5d429f49b80097b602
April <everardo.business@proton.me>
2026-04-08 21:02:15 -0700

    moved my_stock_list.csv to the Data folder

--- src/core.py
+++ src/core.py
@@ -8,8 +8,8 @@ class Core:
         self._set_ticker_list()
 
     def _set_ticker_list(self):
-        if os.path.exists("my_stock_list.csv") and os.path.getsize("my_stock_list.csv") > 0:
-            with open("my_stock_list.csv") as file:
+        if os.path.exists("../data/my_stock_list.csv") and os.path.getsize("../data/my_stock_list.csv") > 0:
+            with open("../data/my_stock_list.csv") as file:
                 self.tickers_list = [line.strip() for line in file if line.strip()]
         else: 
             # if my_stock_list.csv doesn't exist or is empty, a default ticker list is loaded.
